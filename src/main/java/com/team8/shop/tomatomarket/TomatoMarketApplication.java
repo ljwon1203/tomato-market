cecc80725cf0a8127370bbe31053bcf441d2c9d9
i-kemen <lynshu19@gmail.com>
2023-01-16 22:27:42 +0900

    git init

---
+++ src/main/java/com/team8/shop/tomatomarket/TomatoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.team8.shop.tomatomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team8/shop/tomatomarket/TomatoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team8.shop.tomatomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
