eb0d00342afa11beacc1aabab9c0395a03b173f2
mitoconcrete <xogns98@gmail.com>
2023-01-17 19:49:11 +0900

    #13 [feat] 셀러의 정보를 요청했을 경우, 반환하여 주는 API를 제작합니다.

--- src/main/java/com/team8/shop/tomatomarket/service/SellerServiceImpl.java
+++ src/main/java/com/team8/shop/tomatomarket/service/SellerServiceImpl.java
@@ -1,4 +1,30 @@
 package com.team8.shop.tomatomarket.service;
 
+import com.team8.shop.tomatomarket.dto.GetSellerRespDto;
+import com.team8.shop.tomatomarket.entity.Product;
+import com.team8.shop.tomatomarket.entity.ProductCategory;
+import com.team8.shop.tomatomarket.entity.Seller;
+import com.team8.shop.tomatomarket.repository.ProductRepository;
+import com.team8.shop.tomatomarket.repository.SellerRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 public class SellerServiceImpl implements SellerService{
+    private final SellerRepository sellerRepository;
+    private final ProductRepository productRepository;
+
+    @Override
+    public GetSellerRespDto getSeller(Long sellerId) {
+        Seller seller = sellerRepository.findById(sellerId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 판매자 입니다.")
+        );
+
+        List<Product> products = productRepository.findAllById(seller.getId()).orElse(new ArrayList<>());
+
+        return new GetSellerRespDto(seller, products);
+    }
 }
