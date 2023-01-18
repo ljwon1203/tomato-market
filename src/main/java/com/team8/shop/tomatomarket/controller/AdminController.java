60380815ba526b450eba9f8f96a0bb2c47d5960b
mitoconcrete <xogns98@gmail.com>
2023-01-18 12:18:47 +0900

    #2 [feat] 재원님께서 만드실 서비스를 이용하여, admin 권한만 접근 가능한 controller를 제작합니다.

--- src/main/java/com/team8/shop/tomatomarket/controller/AdminController.java
+++ src/main/java/com/team8/shop/tomatomarket/controller/AdminController.java
@@ -1,4 +1,23 @@
 package com.team8.shop.tomatomarket.controller;
 
+import com.team8.shop.tomatomarket.dto.GetSellerRespDto;
+import com.team8.shop.tomatomarket.entity.UserRoleEnum;
+import com.team8.shop.tomatomarket.service.SellerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+    private final SellerServiceImpl sellerService;
+
+    @GetMapping("/sellers")
+    @Secured(UserRoleEnum.Authority.ADMIN)
+    public List<GetSellerRespDto> getSellers(){
+        return sellerService.getSellerList();
+    }
 }
