4966dbea0ea06ae25e4f441d40706f184da543cd
mitoconcrete <xogns98@gmail.com>
2023-01-18 09:24:13 +0900

    #13 [style] 세미콜론추가 및 불필요한 import 를 삭제합니다.

--- src/main/java/com/team8/shop/tomatomarket/controller/UserController.java
+++ src/main/java/com/team8/shop/tomatomarket/controller/UserController.java
@@ -1,16 +1,11 @@
 package com.team8.shop.tomatomarket.controller;
 
-import com.team8.shop.tomatomarket.dto.CreateDisapprovedSellerFormReqDto;
-import com.team8.shop.tomatomarket.dto.SellerRequestDto;
-import com.team8.shop.tomatomarket.security.UserDetailsImpl;
+import com.team8.shop.tomatomarket.dto.*;
 import com.team8.shop.tomatomarket.service.SellerRequestFormService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,7 +15,7 @@ public class UserController {
 
     @PostMapping("/auth/waitings")
     public void createSellerWaiting(@AuthenticationPrincipal UserDetails userDetails, @RequestBody SellerRequestDto dto){
-        CreateDisapprovedSellerFormReqDto serviceRequestDto = new CreateDisapprovedSellerFormReqDto(userDetails.getUsername(), dto.getIntroduce())
+        CreateDisapprovedSellerFormReqDto serviceRequestDto = new CreateDisapprovedSellerFormReqDto(userDetails.getUsername(), dto.getIntroduce());
         sellerRequestFormService.createDisapprovedForm(serviceRequestDto);
     }
 }
