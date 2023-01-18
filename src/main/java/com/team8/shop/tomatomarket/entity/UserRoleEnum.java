439ea81902a4edaaf10d711ca2c60bd778aecd49
haeri <hadi43@naver.com>
2023-01-18 10:21:54 +0900

    #6 [feat] UserRoleEnum에 권한 추가 설정

--- src/main/java/com/team8/shop/tomatomarket/entity/UserRoleEnum.java
+++ src/main/java/com/team8/shop/tomatomarket/entity/UserRoleEnum.java
@@ -1,7 +1,23 @@
 package com.team8.shop.tomatomarket.entity;
 
 public enum UserRoleEnum {
-    CUSTOMER,
-    SELLER,
-    ADMIN
+    ADMIN(Authority.ADMIN),
+    SELLER(Authority.SELLER),
+    CUSTOMER(Authority.CUSTOMER);
+
+    private final String authority;
+
+    UserRoleEnum(String authority){
+        this.authority = authority;
+    }
+
+    public String getAuthority(){
+        return this.authority;
+    }
+
+    public static class Authority{
+        public static final String ADMIN = "ROLE_ADMIN";
+        public static final String SELLER = "ROLE_SELLER";
+        public static final String CUSTOMER = "ROLE_CUSTOMER";
+    }
 }
