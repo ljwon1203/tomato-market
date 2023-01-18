730a91a7a1391b9922b75fe7cdac4e15dc277680
haeri <hadi43@naver.com>
2023-01-18 10:22:57 +0900

    #6 [feat] Spring Security 기능 구현

--- src/main/java/com/team8/shop/tomatomarket/security/UserDetailsServiceImpl.java
+++ src/main/java/com/team8/shop/tomatomarket/security/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.team8.shop.tomatomarket.security;
 
-public class UserDetailsServiceImpl {
+import com.team8.shop.tomatomarket.entity.User;
+import com.team8.shop.tomatomarket.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자가 존재하지 않습니다."));
+
+        return new UserDetailsImpl(user, user.getUsername());
+    }
+
 }
