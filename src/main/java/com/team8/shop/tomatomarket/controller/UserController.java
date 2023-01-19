e572ec41d3593c4bc343cf2f1bca8390b35fd537
mitoconcrete <xogns98@gmail.com>
2023-01-19 12:19:08 +0900

    #16 [fix] service interface 를 사용하지 않고, 상속 객체를 사용하도록 설정

--- src/main/java/com/team8/shop/tomatomarket/controller/UserController.java
+++ src/main/java/com/team8/shop/tomatomarket/controller/UserController.java
@@ -2,20 +2,32 @@ package com.team8.shop.tomatomarket.controller;
 
 import com.team8.shop.tomatomarket.dto.*;
 import com.team8.shop.tomatomarket.service.SellerRequestFormService;
+import com.team8.shop.tomatomarket.service.SellerRequestFormServiceImpl;
+import com.team8.shop.tomatomarket.service.UserService;
+import com.team8.shop.tomatomarket.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final SellerRequestFormService sellerRequestFormService;
+    private final UserServiceImpl userServiceImpl;
+    private final SellerRequestFormServiceImpl sellerRequestFormServiceImpl;
+
+    @PostMapping("/login")
+    public void login(@RequestBody LoginReqDto reqDto, HttpServletResponse response){
+        LoginRespDto loginRespDto = userServiceImpl.login(reqDto);
+    }
+
 
     @PostMapping("/auth/waitings")
     public void createSellerWaiting(@AuthenticationPrincipal UserDetails userDetails, @RequestBody SellerRequestDto dto){
         CreateDisapprovedSellerFormReqDto serviceRequestDto = new CreateDisapprovedSellerFormReqDto(userDetails.getUsername(), dto.getIntroduce());
-        sellerRequestFormService.createDisapprovedForm(serviceRequestDto);
+        sellerRequestFormServiceImpl.createDisapprovedForm(serviceRequestDto);
     }
 }
