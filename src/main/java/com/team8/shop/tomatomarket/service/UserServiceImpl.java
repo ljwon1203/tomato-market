735c52fab02fce50c481943b815e8f63e8d2cea0
mitoconcrete <xogns98@gmail.com>
2023-01-19 16:20:58 +0900

    [HOTFIX] 유저네임으로 유저 검증하는 내부로직 추가.

--- src/main/java/com/team8/shop/tomatomarket/service/UserServiceImpl.java
+++ src/main/java/com/team8/shop/tomatomarket/service/UserServiceImpl.java
@@ -22,7 +22,7 @@ public class UserServiceImpl implements UserService{
         String username = dto.getUsername();
         String password = dto.getPassword();
         // 유저 검증
-        User user = _getUser(userId);
+        User user = _getUser(username);
 
         // 패스워드 검증
         if(!passwordEncoder.matches(password, user.getPassword())){
@@ -89,9 +89,16 @@ public class UserServiceImpl implements UserService{
         return new UserResponseDto(user);
     }
     
-    // 내부 사용 : 유저 검증 
+    // 내부 사용 : 유저 검증 by id
     private User _getUser(Long userId){
         return userRepository.findById(userId).orElseThrow(
                 ()->new IllegalArgumentException("사용자가 존재하지 않습니다."));
     }
+
+    // 내부 사용 : 유저 검증 by username
+    private User _getUser(String username){
+        return userRepository.findByUsername(username).orElseThrow(
+                ()->new IllegalArgumentException("사용자가 존재하지 않습니다."));
+    }
+
 }
