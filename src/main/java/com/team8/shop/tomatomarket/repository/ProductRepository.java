27a38cff08e528785485f43c5cc393bfa2e2eece
haeri <hadi43@naver.com>
2023-01-19 14:46:50 +0900

    #12 [fix] ProductRepository가 JpaRepository를 상속받도록 수정

--- src/main/java/com/team8/shop/tomatomarket/repository/ProductRepository.java
+++ src/main/java/com/team8/shop/tomatomarket/repository/ProductRepository.java
@@ -1,12 +1,13 @@
 package com.team8.shop.tomatomarket.repository;
 
 import com.team8.shop.tomatomarket.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByOrderByModifiedAtDesc();
     List<Product> findAllById(Long id);
 
