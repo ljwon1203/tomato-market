1a966dcebb6a35c40835a24f2b7b9630f1b31d55
haeri <hadi43@naver.com>
2023-01-19 20:32:15 +0900

    #12 [fix] 누락된 중괄호 및 id검증 메소드추가

--- src/main/java/com/team8/shop/tomatomarket/controller/SellerController.java
+++ src/main/java/com/team8/shop/tomatomarket/controller/SellerController.java
@@ -4,7 +4,6 @@ import com.team8.shop.tomatomarket.dto.*;
 import com.team8.shop.tomatomarket.service.SellerService;
 import com.team8.shop.tomatomarket.security.UserDetailsImpl;
 import com.team8.shop.tomatomarket.repository.SellerRepository;
-import com.team8.shop.tomatomarket.entity.User;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -34,11 +33,12 @@ public class SellerController {
     // (판매자)나의 판매상품 조회
     @GetMapping("/sellers/{sellerId}/products")
     public GetSellerRespDto getMyProductList(@PathVariable Long sellerId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        _
+        _checkId(sellerId, userDetails);
         Long userId = userDetails.getUserId();
         // getMyProductList에서 user에 해당하는 productList를 반환해 준다
         return sellerService.getMyProductList(userId);
-       
+    }
+
     // #12 (판매자)판매 상품 등록
     @PostMapping("/sellers/{sellerId}/products")
     public void createProduct(@PathVariable Long sellerId,
@@ -71,10 +71,9 @@ public class SellerController {
 
 
     private void _checkId(Long sellerId, UserDetailsImpl userDetails){
-        Long checkSellerUserId = sellerServiceImpl.getSeller(sellerId).getUser().getId();
+        Long checkSellerUserId = sellerService.getSeller(sellerId).getUser().getId();
         if(!userDetails.isValidId(checkSellerUserId)){
             throw new IllegalArgumentException("등록된 정보와 일치하지 않습니다.");
         }
-
     }
 }
