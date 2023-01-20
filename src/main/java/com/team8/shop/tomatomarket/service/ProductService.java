106f5340171417358ee56d97b630ab78861e9606
haeri <hadi43@naver.com>
2023-01-20 11:37:09 +0900

    #18 [fix] @Service, @RequiredArgsConstructor 애노테이션 추가 및 사용하지 않는 변수 제거

--- src/main/java/com/team8/shop/tomatomarket/service/ProductService.java
+++ src/main/java/com/team8/shop/tomatomarket/service/ProductService.java
@@ -3,21 +3,18 @@ package com.team8.shop.tomatomarket.service;
 import com.team8.shop.tomatomarket.dto.ProductResponseDto;
 import com.team8.shop.tomatomarket.entity.Product;
 import com.team8.shop.tomatomarket.repository.ProductRepository;
-import com.team8.shop.tomatomarket.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
-    private final UserRepository userRepository;
-
-    public ProductService(ProductRepository productRepository, UserRepository userRepository) {
-        this.productRepository = productRepository;
-        this.userRepository = userRepository;
-    }
 
 
     //#14 전체 판매상품 목록 조회
