8a071ce591565809ae0a77aec0c3fd88b928ca9f
Jaewon Lee <ljwon1203@gmail.com>
2023-01-22 01:35:09 +0900

    #20 [refactor] RestApiException 멤버변수 final 선언 및 @RequiredArgsConstructor 어노테이션

--- src/main/java/com/team8/shop/tomatomarket/exception/RestApiException.java
+++ src/main/java/com/team8/shop/tomatomarket/exception/RestApiException.java
@@ -1,12 +1,12 @@
 package com.team8.shop.tomatomarket.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RestApiException {
-    private String errorMessage;
-    private HttpStatus httpStatus;
+    private final String errorMessage;
+    private final HttpStatus httpStatus;
 }
